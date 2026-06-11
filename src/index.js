b553feb623c12949b8bcc1f450c178b70233ee0c
natchanonsarasang <natchanon.sar@dome.tu.ac.th>
2026-06-11 14:43:04 +0700

    Add deploy-command.js and index.js with command registration and interaction handling

---
+++ src/index.js
@@ -0,0 +1,106 @@
+require("dotenv").config();
+
+
+require("./database/init");
+
+
+const {
+Client,
+GatewayIntentBits,
+Collection
+}=require("discord.js");
+
+
+
+const client = new Client({
+
+intents:[
+    GatewayIntentBits.Guilds
+]
+
+});
+
+
+
+client.commands = new Collection();
+
+
+
+const register =
+require("./commands/register");
+
+
+const checkin =
+require("./commands/checkin");
+
+
+const member =
+require("./commands/member");
+
+
+
+client.commands.set(
+"register",
+register
+);
+
+
+client.commands.set(
+"checkin",
+checkin
+);
+
+
+client.commands.set(
+"member",
+member
+);
+
+
+
+client.on(
+"interactionCreate",
+async interaction=>{
+
+
+if(!interaction.isChatInputCommand())
+return;
+
+
+
+const command =
+client.commands.get(
+interaction.commandName
+);
+
+
+
+if(command){
+
+await command.execute(
+interaction
+);
+
+}
+
+
+});
+
+
+
+
+client.once(
+"ready",
+()=>{
+
+console.log(
+`Online : ${client.user.tag}`
+);
+
+});
+
+
+
+client.login(
+process.env.TOKEN
+);
